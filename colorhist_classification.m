5eb6549d88f7d76180c7f16e44491d351a79d956
Sukhbat Batmunkh <68054312+sukhbat112@users.noreply.github.com>
2024-02-29 16:37:56 +0900

    Update colorhist_classification.m

--- colorhist_classification.m
+++ colorhist_classification.m
@@ -1,73 +1,74 @@
-%report1_colorhist.m
-%バトムンフ　スフバト　2110733
+%バトムンフ　スフバト
 
-list={};
-LIST={'hamburger' 'sandwich'};
+list={}; 
+LIST={'hamburger' 'sandwich'}; % list of categories
 %LIST={'pizza' 'rose'};
-DIR0='imgdir/';
+DIR0='imgdir/'; % directory containing each dataset
 
+%loop through each category names to create new list of all filenames
 for i=1:length(LIST)
-    DIR=strcat(DIR0,LIST(i),'/');
-    W=dir(DIR{:});
+    DIR=strcat(DIR0,LIST(i),'/'); % create category directory
+    W=dir(DIR{:}); 
+    % loop through directory contents
     for j=1:size(W)
-        if (strfind(W(j).name,'.jpg'))
-            fn=strcat(DIR{:},W(j).name);
-
-    	    list={list{:} fn};
+        if (strfind(W(j).name,'.jpg')) 
+            fn=strcat(DIR{:},W(j).name); 
+            
+            list={list{:} fn}; 
         end
     end
 end
 
-posimg=list(1:100);
-negimg=list(101:200);
+posimg=list(1:100); % positive images names
+negimg=list(101:200); % negative image names
 
-newlist={posimg{:} negimg{:}};
+newlist={posimg{:} negimg{:}}; % combine
 
-data_colorh=[];
+data_colorh=[]; % color histogram
+% loop through images
+
 for i=1:length(list)
-    X=imread(list{i});
+    X=imread(list{i}); 
+    
+    RED=X(:,:,1); GREEN=X(:,:,2); BLUE=X(:,:,3); % RGB channels
+    X_64=floor(double(RED)/64) *4*4 + floor(double(GREEN)/64) *4 + floor(double(BLUE)/64); % color to 4 base numbers
+    
+    X_64_vec=reshape(X_64,1,numel(X_64)); 
+    h=histc(X_64_vec,(0:63)); % compute histogram
 
-    RED=X(:,:,1); GREEN=X(:,:,2); BLUE=X(:,:,3);
-    X_64=floor(double(RED)/64) *4*4 + floor(double(GREEN)/64) *4 + floor(double(BLUE)/64);
-
-    X_64_vec=reshape(X_64,1,numel(X_64));
-    h=histc(X_64_vec,(0:63));
-
-    h = h / sum(h);      % 要素の合計が１になるように正規化します．
-    data_colorh=[data_colorh; h];
+    h = h / sum(h); 
+    data_colorh=[data_colorh; h]; % add histogram to data ( color histograms of all images)
 end
 
+data_pos = data_colorh(1:100,:); % positive
+data_neg = data_colorh(101:200,:); % negative
 
-data_pos = data_colorh(1:100,:);
-data_neg = data_colorh(101:200,:);
+% 5 fold cross validation
+cv=5; 
+idx=[1:100]; 
 
-cv=5;
-idx=[1:100];
+accuracy=[]; 
 
-accuracy=[];
-% idx番目(idxはcvで割った時の余りがi-1)が評価データ
-% それ以外は学習データ
+% cross-validation loop
 for i=1:cv
+    train_pos=data_pos(find(mod(idx,cv)~=(i-1)),:); 
+    eval_pos =data_pos(find(mod(idx,cv)==(i-1)),:); 
+    train_neg=data_neg(find(mod(idx,cv)~=(i-1)),:); 
+    eval_neg =data_neg(find(mod(idx,cv)==(i-1)),:); 
 
-    train_pos=data_pos(find(mod(idx,cv)~=(i-1)),:);
-    eval_pos =data_pos(find(mod(idx,cv)==(i-1)),:);
-    train_neg=data_neg(find(mod(idx,cv)~=(i-1)),:);
-    eval_neg =data_neg(find(mod(idx,cv)==(i-1)),:);
+    train_data=[train_pos; train_neg]; 
+    eval_data=[eval_pos; eval_neg]; 
 
-    train_data=[train_pos; train_neg];
-    eval_data=[eval_pos; eval_neg];
+    train_label=[ones(80,1); ones(80,1)*(-1)]; % training labels
+    eval_label =[ones(20,1); ones(20,1)*(-1)]; % evaluation labels
 
-    train_label=[ones(80,1); ones(80,1)*(-1)];
-    eval_label =[ones(20,1); ones(20,1)*(-1)];
-
-
-    %%線形カーネルを用いる
+    % train linear SVM model
     model = fitcsvm(train_data, train_label,'KernelFunction','linear');
-    [predicted_label, scores] = predict(model, eval_data);
+    [predicted_label, scores] = predict(model, eval_data); % Predict labels
 
-    ac = numel(find(eval_label==predicted_label))/numel(eval_label);
-    accuracy=[accuracy ac];
+    ac = numel(find(eval_label==predicted_label))/numel(eval_label); % compute accuracy
+    accuracy=[accuracy ac]; 
 end
 
-accuracy;
-avg_accuracy = mean(accuracy)
+accuracy; 
+avg_accuracy = mean(accuracy) % compute average accuracy
