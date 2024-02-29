a72516ee09ddad012788ecf357d889de9c62e650
Sukhbat Batmunkh <68054312+sukhbat112@users.noreply.github.com>
2024-02-29 16:39:49 +0900

    Update data_collect.m

--- data_collect.m
+++ data_collect.m
@@ -1,5 +1,5 @@
-%data_collect.m
-%バトムンフ　スフバト　2110733
+%バトムンフ　スフバト　
+%Sukhbat Batmunkh
 
 LIST={'pizza' 'hamburger' 'rose' 'sandwich' 'rose_noise' 'hamburger_noise'};
 
@@ -7,17 +7,16 @@ DIR0='imgdir/';
 
 urllists = {'urllist_pizza.txt' 'urllist_hamburger.txt' 'urllist_rose.txt' 'urllist_sandwich.txt' 'urllist_rose_noise.txt' 'urllist_hamburger_noise.txt'};
 
-
+% Loop through categories
 for i=1:length(LIST)
-
-    OUTDIR=strcat(DIR0,LIST{i},'/');
-    mkdir(OUTDIR);
+    OUTDIR=strcat(DIR0,LIST{i},'/');   % output directory
+    mkdir(OUTDIR);                     % create directory
     
-    list=textread(urllists{i},'%s');
-
+    list=textread(urllists{i},'%s'); % read URL list
+    
+    % lop through URLs
     for j=1:size(list,1)
-        fname=strcat(OUTDIR,num2str(j,'%04d'),'.jpg')
-        websave(fname,list{j});
+        fname=strcat(OUTDIR,num2str(j,'%04d'),'.jpg'); % img file name
+        websave(fname,list{j}); % download img
     end
-
 end
